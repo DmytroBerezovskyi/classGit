bcd144de0ef9021ccd9caaa165a09153560f552f
DmytroBerezovskyi <berezovscky.dmitrij1@gmail.com>
2021-08-28 08:14:02 +0300

    Full save

---
+++ class.py
@@ -0,0 +1,16 @@
+print('Hello world', 456, 45.78, True)
+print('Hello world')
+print(456)
+print(45.78)
+print(True)
+print('Hello world', 456, 45.78, True, sep=' <--> ', end=' ::: ')
+print('Hello world', 456, 45.78, True, sep=' <--> ')
+
+a = "Hello 'World'!"
+print (a)
+s = 'tgrtgrgtgrtgrtgrrgrtgrtrgrtgtrgtrtgrtgtrgt\
+trgrtgrrtsss'
+print(s)
+
+input(a)
+
